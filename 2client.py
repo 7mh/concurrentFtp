bc510e2fdbb0d4d92a88b15ace6da1108a08ffab
7mh <muhashmi0@gmail.com>
2020-07-14 05:04:16 -0400

    Sever works

--- 2client.py
+++ 2client.py
@@ -3,7 +3,7 @@ import socket
 import os
 import sys
 from hashlib import md5
-
+from md5sum import md5sum
 
 
 SOURCEPATH = "/u1/h3/hashmi/public_html/source"
@@ -18,36 +18,37 @@ SERVER = "127.0.0.1"                 #SELECT HOME ADDR or GET MACHINE IP
 #SERVER = socket.gethostbyaddr(socket.gethostname())[2][0]
 
 PORT = 8089
-HEADERSIZE = 20
+SIZEl = 20              #20:filesize, 255:name, 32:CSum 168:EXTRA
+NAMEl = 255
+CHECKSUMl = 32
+EXTRAl = 168
+HEADERSIZE =  SIZEl+NAMEl+CHECKSUMl+EXTRAl
+
+#HEADERSIZE = 20 + 255 + 200     #20:filesize, 255:name, 32:CSum 168:EXTRA
 PACKETSIZE = 128* filehash.block_size   #coz md5 has 128*64 digest blks
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 #client.sendall(bytes("This is from Client",'UTF-8'))
 print(os.getcwd())
 print(f"number of files to read {len(allfiles)}")
-i = 2
-packetCount = 0
-
+i = 3
 
 while True:
-#for i in range(len(allfiles)):
-
+    packetCount = 0
+    HASH = md5sum(allfiles[i])
+    print(f"HASH : {HASH}")
     with open(allfiles[i],"r") as fd:
-            HEAD = f"{filesize[i]:<{HEADERSIZE}}"
-            msg = HEAD + fd.read(PACKETSIZE - HEADERSIZE)
-            chunkByte = bytes(msg, 'utf-8')
-            client.sendall(chunkByte)
-            filehash.update(chunkByte)
-            print(len(msg))
-            for chunk in iter(lambda: fd.read(PACKETSIZE), ""):
+        HEAD = f"{filesize[i]:<20}{allfiles[i]:<255}{HASH:<200}"
+        msg = HEAD + fd.read(PACKETSIZE - HEADERSIZE)
+        chunkByte = bytes(msg, 'utf-8')
+        client.sendall(chunkByte)
+        print(len(msg))
+        for chunk in iter(lambda: fd.read(PACKETSIZE), ""):
                 packetCount += 1
                 chunkByte = bytes(chunk, 'utf-8')
                 client.sendall(chunkByte)
-                filehash.update(chunkByte)
-    print("Done reading")
-    HASH = filehash.hexdigest()
+    print(f"{allfiles[i]} Done reading")
     print(HASH)
-    client.sendall(bytes(HASH, 'utf-8'))
     input()
 
     '''
--- server.py
+++ server.py
@@ -4,27 +4,24 @@ import socket
 import threading
 import os
 from hashlib import md5
-import md5sum
+from md5sum import md5sum
+import pdb
 
 
 LOCALHOST = "127.0.0.1"
 #LOCALHOST = socket.gethostbyaddr(socket.gethostname())[2][0]
 PORT = 8089
-HEADERSIZE = 20
+#HEADERSIZE = 20
+SIZEl = 20              #20:filesize, 255:name, 32:CSum 168:EXTRA
+NAMEl = 255
+CHECKSUMl = 32
+EXTRAl = 168
+HEADERSIZE =  SIZEl+NAMEl+CHECKSUMl+EXTRAl
 filehash = md5()
 PACKETSIZE = 128* filehash.block_size   #coz md5 has 128*64 digest blks
 SOURCEPATH = "/u1/h3/hashmi/public_html/dest"
 os.chdir(SOURCEPATH)
 
-
-'''
-def md5sum(filename):
-    hash = md5()
-    with open(filename, "rb") as f:
-        for chunk in iter(lambda: f.read(128 * hash.block_size), b""):
-            hash.update(chunk)
-    return hash.hexdigest()
-'''
 
 class ClientThread(threading.Thread):
     def __init__(self,clientAddress,clientsocket, num):
@@ -40,36 +37,50 @@ class ClientThread(threading.Thread):
         new_msg = True
         count  = 1
         byteRecv = 0
+        filesize = 0
         try:
             fd = open(self.filenum, 'a+')
         except:
             print("ERROR OPENING FILE")
 
         while True:
+            #condition when a small file occupies only 1 Packet
+            if byteRecv != 0 and filesize != 0 and byteRecv >= filesize:
+                break
+
             data = self.csocket.recv(PACKETSIZE)
             msg = data.decode()
-            if len(msg) < PACKETSIZE:
-                print(msg[:100])
-                print(msg[-100:])
+
+
             if new_msg == False:
                 byteRecv += len(msg)
                 fd.write(msg)
+                if len(msg) == 0:               #when client closes socket
+                    print("RECVD empty msg")
+                    break
+                if byteRecv >= filesize:
+                    print("Transfer complete")
+                    break
             if new_msg:
-                filesize = int(msg[:HEADERSIZE])
-                print(f"new file size {filesize}")
+                HEADER = msg[:HEADERSIZE]
+                print(f"HEADER : {HEADER}")
+                filesize = int(HEADER[:SIZEl])
+                filename = HEADER[SIZEl: (SIZEl+NAMEl)]
+                filechksum = HEADER[(SIZEl+NAMEl):(SIZEl+NAMEl+32)]
+
+                print(f"new file size {filesize}, {filename}, {filechksum}")
                 byteRecv = len(msg) - HEADERSIZE
                 fd.write(msg[HEADERSIZE:])
                 new_msg = False
             print (f"{count} read from client LEN:{len(msg)} RECV:{byteRecv}", msg[:40])
             count += 1
+            #if len(msg) < PACKETSIZE:   #testing
+            #    break
             #if msg=='bye':
-            if len(msg) == 0:               #when client closes socket
-                print("RECVD empty msg")
-                break
 
             #if byteRecv >= filesize :       #Paranioa
             #    break
-
+            '''
             if (byteRecv + PACKETSIZE) >= filesize :
                 data = self.csocket.recv(PACKETSIZE)
                 msg = data.decode()
@@ -82,7 +93,7 @@ class ClientThread(threading.Thread):
                 HASH = msg[(left): (left +32)]
                 print("HASH is: ",HASH)
                 break
-
+            '''
                 #print(msg[:left])
 
             #self.csocket.send(bytes(msg,'UTF-8'))
@@ -90,6 +101,15 @@ class ClientThread(threading.Thread):
         print ("Client at ", clientAddress , " disconnected...")
         self.csocket.close()
         fd.close()
+        filename = filename.rstrip()
+        os.rename(self.filenum, filename)
+        chkSum = md5sum(filename)
+        #print(chkSum, filechksum == chkSum)
+        #pdb.set_trace()
+        if filechksum == chkSum:
+            print(filename, "CHECKSUM PASSED !")
+        else:
+            print(filename, "CHECKSUM failED !")
 
 
 if __name__ == "__main__":
